74933c7e7b91e0ceb2c5472570e30d6c313af14e
implicit-image <blaz.nie@protonmail.com>
2020-11-01 00:29:29 +0100

    initial commit

---
+++ init.el
@@ -0,0 +1,36 @@
+(setq inhibit-startup-screen t)
+(setq visible-bell 1)
+
+
+(tool-bar-mode 0)
+(ido-mode 1)
+
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  ;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes '(tango-dark))
+ '(custom-safe-themes
+   '("5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
+ '(package-selected-packages '(gruber-darker-theme smex)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(setq backup-directory-alist '(("." . "~/.emacs_saves")))
