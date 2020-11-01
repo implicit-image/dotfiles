9835b613ded4cbb42146959fb3e71e95192c6ceb
implicit-image <blaz.nie@protonmail.com>
2020-11-01 23:11:25 +0100

    configuring haskell mode

--- init.el
+++ init.el
@@ -6,15 +6,29 @@
 (ido-mode 1)
 
 (require 'package)
-
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
+;;--smex---------------------------------------------------------------------
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
   ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;;--haskell mode--------------------------------------------------------------
+(define-key haskell-mode-map (kbd "<f5>") 'haskell-navigate-imports)
+(define-key haskell-mode-map (kbd "<f8>") 'haskell-mode-format-imports)
+
+;;hooks
+(add-hook 'haskell-mode-hook 'haskell-interactive-mode)
+(add-hook 'haskell-mode-hook
+         (lambda ()
+            (set (make-local-variable 'company-backends)
+                 (append '((company-capf company-dabbrev-code))
+                         company-backends))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -26,7 +40,8 @@
  '(custom-enabled-themes '(tango-dark))
  '(custom-safe-themes
    '("5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
- '(package-selected-packages '(gruber-darker-theme smex)))
+ '(package-selected-packages
+   '(company-ghci company lsp-haskell lsp-mode lsp-ui haskell-mode gruber-darker-theme smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,3 +49,5 @@
  ;; If there is more than one, they won't work right.
  )
 (setq backup-directory-alist '(("." . "~/.emacs_saves")))
+
+
