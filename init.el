fd5bee3489b3280ab639f04a12de15ab8bac50e1
implicit-image <blaz.nie@protonmail.com>
2020-11-03 16:22:28 +0100

    js setup done

--- init.el
+++ init.el
@@ -9,15 +9,14 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;;(add-hook 'after-init-hook 'global-company-mode)
 
-;;--smex---------------------------------------------------------------------
+;;smex
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
   ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
-;;--haskell mode--------------------------------------------------------------
+;;haskell mode
 (require 'lsp)
 (require 'lsp-mode)
 (require 'lsp-haskell)
@@ -25,31 +24,55 @@
 (setq haskell-process-log t)
 (setq haskell-process-type "stack-ghci")
 
-;;(define-key haskell-mode-map (kbd "<f5>") 'haskell-navigate-imports)
-;;(define-key haskell-mode-map (kbd "<f8>") 'haskell-mode-format-imports)
-
-;;hooks
-;;(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook #'lsp)
 (add-hook 'haskell-mode-hook 'lsp-mode)
 (add-hook 'haskell-mode-hook 'company-mode)
 
 
-;;--rust----------------------------------------------------
+;;rust
 (require 'lsp-mode)
 (setq lsp-rust-server 'rust-analyzer)
 (add-hook 'rust-mode-hook 'lsp-mode)
 (add-hook 'rust-mode-hook 'company-mode)
 
-(setq company-idle-delay 0.2)
+;;company
+(setq company-idle-delay -1)
 (setq company-minimum-prefix-length 1)
+(setq company-show-numbers t)
+(setq company-tooltip-offset-display -1)
 
+
+
+
+	  
 (global-set-key (kbd "TAB") #'company-indent-or-complete-common) ;
 (setq company-tooltip-align-annotations t)
 (global-set-key (kbd "M-p") 'ace-window)
 (global-display-line-numbers-mode 1)
 
 
+;;rjsx
+(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+
+
+
+;;tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+
+
+;;--custom variables
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -61,7 +84,7 @@
  '(custom-safe-themes
    '("5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
  '(package-selected-packages
-   '(tide js2-mode web-mode ace-window company-lsp company-racer flycheck flycheck-rust racer cargo rust-mode ac-haskell-process company-ghci company lsp-haskell lsp-mode lsp-ui haskell-mode gruber-darker-theme smex)))
+   '(typescript-mode rjsx-mode tide js2-mode web-mode ace-window company-lsp company-racer flycheck flycheck-rust racer cargo rust-mode ac-haskell-process company-ghci company lsp-haskell lsp-mode lsp-ui haskell-mode gruber-darker-theme smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -69,5 +92,4 @@
  ;; If there is more than one, they won't work right.
  )
 (setq backup-directory-alist '(("." . "~/.emacs_saves")))
-
 
