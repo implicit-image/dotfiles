b3d3d70249a902683769de312dfd3c8b730e0b86
Błażej Niewiadomski <blaz.nie@protonmail.com>
2021-02-22 04:08:18 +0100

    layout order fixed

--- .profile
+++ .profile
@@ -3,3 +3,6 @@ export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
 export CALIBRE_USE_DARK_PALETTE=1
 source "$HOME/.cargo/env"
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
--- init.el
+++ init.el
@@ -7,6 +7,7 @@
 (setq org-startup-truncated nil)
 (setq inhibit-startup-screen t)
 (setq visible-bell 1)
+(setq create-lockfiles nil)
 ;;(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
 (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
 (global-display-line-numbers-mode 1)
@@ -15,7 +16,19 @@
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (ido-mode 1)
+(show-paren-mode 1)
+(electric-pair-mode 1)
 
+;;indent
+
+;; (setq highlight-indent-guides-character '"|")
+;; (setq highlight-indent-guides-method 'character)
+;; (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+
+
+;; agenda view
+
+;;(add-to-list 'org-agenda-files '(("." . "~/.emacs.d/agenda-files")))
 
 
 ;;smex
@@ -57,6 +70,7 @@
 (add-hook 'rust-mode-hook #'setup-rust-mode)
 (add-hook 'rust-mode-hook #'lsp) 
 
+;;(define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
 
 
 ;;company
@@ -95,11 +109,22 @@
 ;;(add-hook 'pdf-view-mode-hook #'pdf-tools-install)
 
 ;;javascript
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-mode))
-(add-hook 'js2-mode-hook #'setup-tide-mode)
+;;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;;(add-hook 'js2-mode-hook #'setup-tide-mode)
 
 
+;;react
 
+(defun setup-react-mode()
+  (flycheck-mode +1)
+  (eldoc-mode +1)
+  (company-mode +1)
+  )
+
+(setq js-indent-level 2)
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+(add-hook 'rjsx-mode-hook #'setup-react-mode)
 
 ;;json
 (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
@@ -126,6 +151,8 @@
 
 ;;vterm
 
+;;dired
+(add-hook 'dired-mode-hook 'dired-git-mode)
 
 
 
@@ -137,13 +164,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
+   '("46b2d7d5ab1ee639f81bde99fcd69eb6b53c09f7e54051a591288650c29135b0" "5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
  '(package-selected-packages
-   '(org-bullets use-package pdfgrep pdf-tools pdf-view-restore lua-mode magit org-present-remote vterm org-beautify-theme org-present ox-epub org-brain org-noter org json-mode typescript-mode rjsx-mode tide js2-mode web-mode ace-window company-lsp company-racer flycheck flycheck-rust racer cargo rust-mode ac-haskell-process company-ghci company lsp-haskell lsp-mode lsp-ui haskell-mode gruber-darker-theme smex)))
+   '(image+ iscroll look-mode dired-git image-dired+ company-c-headers company-shell company-web css-eldoc scss-mode monokai-alt-theme highlight-indent-guides org-bullets use-package pdfgrep pdf-tools pdf-view-restore lua-mode magit org-present-remote vterm org-beautify-theme org-present ox-epub org-brain org-noter org json-mode typescript-mode rjsx-mode tide js2-mode web-mode ace-window company-lsp company-racer flycheck flycheck-rust racer cargo rust-mode ac-haskell-process company-ghci company lsp-haskell lsp-mode lsp-ui haskell-mode gruber-darker-theme smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
