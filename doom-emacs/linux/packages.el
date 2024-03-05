3cadcd2eb6f08fec2d77dfaf249e256a34b16d02
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-03-06 00:59:41 +0100

    Add tailwind-css package

--- doom-emacs/linux/packages.el
+++ doom-emacs/linux/packages.el
@@ -2,20 +2,28 @@
 ;;; $DOOMDIR/packages.el
 
 
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-                                        ;(package! some-package)
 
 (package! cheat-sh.el
   :recipe (:host github :repo "davep/cheat-sh.el"
            :files ("cheat-sh.el")))
-(package! gruber-darker-theme
+(package! doom-gruber-darker
   :recipe (:host github
-           :repo "implicit-image/gruber-darker-theme"
-           :files ("gruber-darker-theme.el")))
+           :repo "implicit-image/doom-gruber-darker"
+           :files ("doom-gruber-darker-theme.el")))
+;; (package! doom-gruber-darker
+;;   :recipe (:host github
+;;            :repo "implicit-image/doom-gruber-darker"))
 (package! twittering-mode
-  :recipe (:host gtihub
+  :recipe (:host github
            :repo "hayamiz/twittering-mode"
            :files ("twittering-mode.el")))
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
+(package! gruber-darker-theme
+  :recipe (
+           :host github
+           :repo "rexim/gruber-darker-theme"
+           :files ("gruber-darker-theme.el")))
+(package! d-mode)
 (package! calibredb)
 (package! nov)
 (package! color)
