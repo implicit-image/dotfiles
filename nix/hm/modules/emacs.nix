f1f666f1b8824891684058cc73ce6491a560f252
Błażej Niewiadomski <blaz.nie@protonmail.com>
2026-01-06 04:20:04 +0100

    add lsp-bridge emacs package

--- nix/hm/modules/emacs.nix
+++ nix/hm/modules/emacs.nix
@@ -11,42 +11,25 @@
         ref = "master";
       };
     });
-
-    #   # configureFlags = (builtins.filter
-    #   #   (option: !(option == "--with-cairo"))
-    #   #   prev.configureFlags) ++ [ "--with-x-toolkit=gtk3" "--with-xwidgets=yes" ];
-    #   buildInputs = with pkgs; [
-    #     gtk3
-    #     (webkitgtk_6_0.overrideAttrs (final: prev: {
-    #       version = "2.41.90";
-    #       src = fetchurl {
-    #         url = "https://webkitgtk.org/releases/webkitgtk-2.41.90.tar.xz";
-    #         hash = "sha256-0frBmcORsOq8X58OK5DeTbOhSFtAKEhHsYD8apVjT+k=";
-    #       };
-    #       # buildInputs = with pkgs; [ pcre2 libxdmcp ];
-    #       buildInputs = (with pkgs; [ gtk3 pcre2 xorg.libXdmcp libgcrypt libgpg-error libsysprof-capture harfbuzzFull libepoxy libwpe libwpe-fdo xorg.libXt openjpeg libunicode ]) ++ prev.buildInputs;
-    #     }))
-    #   ];
-    # });
-    # package.emacs-git;
     extraPackages = (epkgs: (with epkgs; [
-    treesit-grammars.with-all-grammars
-    (melpaBuild {
-      ename = "reader";
-      pname = "emacs-reader";
-      version = "20250630";
-      src = pkgs.fetchFromGitea {
-        domain = "codeberg.org";
-        owner = "divyaranjan";
-        repo = "emacs-reader";
-        rev = "9d62d26fe4ae63e5cecf57bc399b20f7feefb620"; # replace with 'tag' for stable
-        hash = "sha256-hkRa52PYfBG090jior3GPOeZyftwmpr2Q7jPKFHsR88=";
-      };
-      files = ''(:defaults "render-core.so")'';
-      nativeBuildInputs = [ pkgs.pkg-config ];
-      buildInputs = with pkgs; [ gcc mupdf gnumake pkg-config gtk3 ];
-      preBuild = "make clean all";
-    })
+      lsp-bridge
+      treesit-grammars.with-all-grammars
+      (melpaBuild {
+        ename = "reader";
+        pname = "emacs-reader";
+        version = "20250630";
+        src = pkgs.fetchFromGitea {
+          domain = "codeberg.org";
+          owner = "divyaranjan";
+          repo = "emacs-reader";
+          rev = "9d62d26fe4ae63e5cecf57bc399b20f7feefb620"; # replace with 'tag' for stable
+          hash = "sha256-hkRa52PYfBG090jior3GPOeZyftwmpr2Q7jPKFHsR88=";
+        };
+        files = ''(:defaults "render-core.so")'';
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = with pkgs; [ gcc mupdf gnumake pkg-config gtk3 ];
+        preBuild = "make clean all";
+      })
     ]));
   };
   xdg.desktopEntries = {
