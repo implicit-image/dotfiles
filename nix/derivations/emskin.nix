b9bd0db232519c61434e9a20fc46aa36592e2534
Błażej Niewiadomski <blaz.nie@protonmail.com>
2026-04-28 02:53:15 +0200

    add emskin lock file

--- nix/derivations/emskin.nix
+++ nix/derivations/emskin.nix
@@ -4,26 +4,35 @@ let
 
   # Fetch the patched smithay dependency from the custom branch.
   # Replace `rev` with a specific commit hash for reproducibility.
-  smithay = pkgs.fetchGit {
+  smithay = builtins.fetchGit {
     url = "https://github.com/emskin/smithay.git";
     ref = "emskin-patches";
     rev = "4d3e1c86f1b0a4fb7df876db0af849f4bba06f88";   # <-- required for deterministic builds
   };
+  emskin = builtins.fetchGit {
+      url = "https://github.com/emskin/emskin";
+      ref = "main";
+      rev = "6c26440dfbb6ae1e3dff9e8f14902ff4157d9a0c";
+  };
 in
-  rustPlatform.buildRustPackage rec {
+  rustPlatform.buildRustPackage (final: {
     pname = "emskin";
     version = "0.3.12";
 
-    src = ./.;
+    src = emskin ;
 
     # Use the Cargo.lock that must be present in the source tree.
     # If smithay is not already in the lock file, you need to provide its output hash:
     cargoLock = {
-      lockFile = ./Cargo.lock;
+      lockFile = ./emskin.lock;
       # outputHashes = {
       #   "smithay-0.3.0" = "sha256-...";   # calculate with `nix run nixpkgs#nix-prefetch-git ...`
       # };
     };
+
+    # postPatch  = ''
+    #   ln -s ${./Cargo.lock} Cargo.lock
+    # '';
 
     nativeBuildInputs = with pkgs; [ pkg-config ];
     buildInputs = with pkgs; [ wayland libxkbcommon mesa ];
@@ -37,4 +46,4 @@ in
       license = licenses.gpl3;
       maintainers = [ maintainers.le ];
     };
-  }
+  })
